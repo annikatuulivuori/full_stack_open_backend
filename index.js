bf5b752d885e13054ade06ec7a62349aefee5506
annika <atuulivuo@gmail.com>
2023-06-19 16:36:02 +0300

    3.5: phonebook backend step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
         name: "Arto Hellas", 
@@ -46,6 +48,31 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
     }
     
+})
+
+const generateId = () => {
+    const id = Math.floor(Math.random() * 100 + 1)
+    return id
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number:body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
