3d5e67edf59e855d569345daf2e3152475c3d03c
annika <atuulivuo@gmail.com>
2023-06-19 14:23:02 +0300

    3.3: phonebook backend step3

--- index.js
+++ index.js
@@ -24,19 +24,31 @@ let persons = [
     }
 ]
   
-  app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response) => {
     response.json(persons)
-  })
+})
 
-  app.get('/info', (request, response) => {
+app.get('/info', (request, response) => {
     const length = persons.length
     const currentDate = new Date().toUTCString()
     const msg = `Phonebook has info for ${length} people.<br>${currentDate}`
 
     response.send(`<p>${msg}</p>`)
-  })
-  
-  const PORT = 3001
-  app.listen(PORT, () => {
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+    
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
+})
