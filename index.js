92e5bf60bc9a28c8cff26ac725c18818e88ceb3e
annika <atuulivuo@gmail.com>
2023-06-27 16:34:17 +0300

    3.10: phonebook backend step10

--- index.js
+++ index.js
@@ -38,11 +38,6 @@ let persons = [
         name: "Mary Poppendieck", 
         number: "39-23-6423122",
         id: 4
-    },
-    { 
-        name: "lollero", 
-        number: "39-23-6423122",
-        id: 5
     }
 ]
   
@@ -109,7 +104,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
